21abfccc530275511baafa6b84452cf8107aad7b
chenchaoyun <chenchaoyun@megvii.com>
2018-07-12 19:08:35 +0800

    modify datasource to hikari
    Change-Id: I634584b12b78bc84f39378013a721fc64d3d74de

--- book-service-common/src/main/java/com/cyc/config/DruidStatFilter.java
+++ book-service-common/src/main/java/com/cyc/config/DruidStatFilter.java
@@ -1,12 +1,13 @@
 package com.cyc.config;
 
-import javax.servlet.annotation.WebFilter;
+/*import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebInitParam;
 
 import com.alibaba.druid.support.http.WebStatFilter;
 
 @WebFilter(filterName = "druidWebStatFilter", urlPatterns = "/*", initParams = {
-        /** 忽略资源 */
+        *//** 忽略资源 *//*
         @WebInitParam(name = "exclusions", value = "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*") })
 public class DruidStatFilter extends WebStatFilter {
 }
+*/
