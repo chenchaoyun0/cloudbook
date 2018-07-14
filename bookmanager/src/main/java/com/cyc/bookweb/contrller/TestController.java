0061a52b5442f714e97961d04bfe7dd938c2e201
chenchaoyun <chenchaoyun@megvii.com>
2018-07-14 17:33:52 +0800

    test session syn
    Change-Id: I1a0232bf69b23739e6a67ff578341b69a7c205d8

--- bookmanager/src/main/java/com/cyc/bookweb/contrller/TestController.java
+++ bookmanager/src/main/java/com/cyc/bookweb/contrller/TestController.java
@@ -1,26 +1,43 @@
 package com.cyc.bookweb.contrller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSONObject;
+import com.cyc.common.utils.LogUtil;
+import com.cyc.common.utils.apaddress.IPUtils;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/")
+@Slf4j
 public class TestController {
 
   @RequestMapping(value = "/getSessionId")
-  @ResponseBody
-  public String getSessionId(HttpServletRequest request) {
+  public void getSessionId(HttpServletRequest request, HttpServletResponse response) {
 
-    Object o = request.getSession().getAttribute("springboot");
-    if (o == null) {
-      o = "spring boot 牛逼了!!!有端口" + request.getLocalPort() + "生成";
-      request.getSession().setAttribute("springboot", o);
+    try {
+      String ipAddr = IPUtils.getIpAddr(request);
+      Object o = request.getSession().getAttribute("springboot");
+      if (o == null) {
+        o = "spring boot redis session 测试,由 ip:" + ipAddr + "端口:" + request.getLocalPort() + "生成";
+        request.getSession().setAttribute("springboot", o);
+      }
+      JSONObject json = new JSONObject();
+      json.put("ipAddress", ipAddr);
+      json.put("port", request.getLocalPort());
+      json.put("sessionId", request.getSession().getId());
+      json.put("sessionData", o);
+      String formatAsJSON = LogUtil.formatAsJSON(json.toJSONString());
+      response.setCharacterEncoding("GBK");
+      response.getWriter().write(formatAsJSON);
+    } catch (Exception e) {
+      log.error("visitors error:{}", e);
     }
-
-    return "端口=" + request.getLocalPort() + " sessionId=" + request.getSession().getId() + "<br/>" + o;
   }
 
 }
