06de136207e58b9df237293c4aba7138fbcffe3b
chenchaoyun <chenchaoyun@megvii.com>
2018-07-14 17:33:52 +0800

    test session syn
    Change-Id: I1a0232bf69b23739e6a67ff578341b69a7c205d8

--- bookmanager/src/main/java/com/cyc/bookweb/contrller/TestController.java
+++ bookmanager/src/main/java/com/cyc/bookweb/contrller/TestController.java
@@ -1,26 +1,37 @@
 package com.cyc.bookweb.contrller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cyc.common.utils.apaddress.IPUtils;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/")
+@Slf4j
 public class TestController {
 
   @RequestMapping(value = "/getSessionId")
-  @ResponseBody
-  public String getSessionId(HttpServletRequest request) {
+  public void getSessionId(HttpServletRequest request, HttpServletResponse response) {
 
-    Object o = request.getSession().getAttribute("springboot");
-    if (o == null) {
-      o = "spring boot 牛逼了!!!有端口" + request.getLocalPort() + "生成";
-      request.getSession().setAttribute("springboot", o);
+    try {
+      String ipAddr = IPUtils.getIpAddr(request);
+      Object o = request.getSession().getAttribute("springboot");
+      if (o == null) {
+        o = "spring boot 牛逼了!!!由ip:"+ipAddr+"端口:" + request.getLocalPort() + "生成";
+        request.getSession().setAttribute("springboot", o);
+      }
+      String result
+        = "ip:" + ipAddr + ",端口:" + request.getLocalPort() + ",sessionId=" + request.getSession().getId() + ";session data:{" + o+"}";
+      response.setCharacterEncoding("GBK");
+      response.getWriter().write(result);
+    } catch (Exception e) {
+      log.error("visitors error:{}", e);
     }
-
-    return "端口=" + request.getLocalPort() + " sessionId=" + request.getSession().getId() + "<br/>" + o;
   }
 
 }
